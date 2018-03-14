7b1beb1689a2b6968931619bbf6ff7deff954ce1
Lucas Chaufournier <lucasch@cs.umass.edu>
2018-03-14 11:41:36 -0400

    First commit

---
+++ src/kale.go
@@ -0,0 +1,21 @@
+package kale;
+
+
+import (
+	_ "encoding/json"
+	_"github.com/gorilla/mux"
+	"log"
+	_"net/http"
+)
+
+
+type Kale struct {
+
+}
+
+func (k Kale) plant(){
+
+	log.Print("Planted A New Kale")
+
+
+}
---
+++ src/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "kale"
+
+
+
+
+func main(){
+
+	var k = new(kale.Kale)
+
+	k.plant()
+}
